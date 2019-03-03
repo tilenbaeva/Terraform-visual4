db0961e6bb1093461341dd78949e8af357235280
Aiza Tilenbaeva <tilenbaevaa@gmail.com>
2019-03-03 15:36:34 -0600

    change

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "example" {
     instance_type = "${var.instance_type}"
     key_name = "${var.keyname}"
     subnet_id = "${var.subnet_id}"
-    associate_public_ip_address = "yes"
+    associate_public_ip_address = True
 
 
 }
