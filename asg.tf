f02a75baef28d4638a70e1c4a79be49864e807ee
Aiza Tilenbaeva <tilenbaevaa@gmail.com>
2019-03-03 16:06:00 -0600

    ch

--- asg.tf
+++ asg.tf
@@ -1,6 +1,6 @@
 resource "aws_autoscaling_group" "dev" {
     name = "dev"
-    launch_configuration = "${aws_autoscaling_group.dev.name}"
+    launch_configuration = "${aws_launch_configuration.dev.dev.name}"
     min = "${var.min_size}"
     max = "${var.max_size}"
 }
