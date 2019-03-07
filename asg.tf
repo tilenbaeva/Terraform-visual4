17388d344dec96745c1f586c3e13356ae3c6a6c4
Aiza Tilenbaeva <tilenbaevaa@gmail.com>
2019-03-06 18:32:34 -0600

    auto scaling group, internet gateway has changed

--- asg.tf
+++ asg.tf
@@ -4,4 +4,5 @@ resource "aws_autoscaling_group" "dev" {
     min_size = "${var.min_size}"
     max_size = "${var.max_size}"
     availability_zones = ["eu-west-1b", "eu-west-1a", "eu-west-1c"]
+    vpc_zone_identifier = ["${aws_subnet.dev1.id}", "${aws_subnet.dev2.id}"]
 }
