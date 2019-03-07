d9982704dc0805edcee16e7e0a10707dac4e5643
Aiza Tilenbaeva <tilenbaevaa@gmail.com>
2019-03-06 18:34:54 -0600

    subnet association

--- subnet_association.tf
+++ subnet_association.tf
@@ -1,4 +1,4 @@
 resource "aws_route_table_association" "dev" {
-    subnet_id = "${aws_subnet.dev.id}"
-    route_table_id = "${aws_route_table.dev1.id}"  
+    subnet_id = "${aws_subnet.dev1.id}"
+    route_table_id = "${aws_internet_gateway.gw.id}"  
 }
